d37f4361440edf09db82312ed84782da46d35e7d
Kirill Myagkov <kirillmyagkov17@gmail.com>
2020-03-23 18:40:08 +0300

    Add layout

---
+++ components/layout/Navigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navigation">
+    <h1 class="navigation__title">SSR Blog</h1>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.navigation {
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid #909399;
+}
+
+.navigation__title {
+  margin: 1rem 0;
+  font-size: 2rem;
+  color: #333;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <el-container>
+    <el-header>
+      <AppNavigation />
+    </el-header>
+    <el-main>
+      <nuxt />
+    </el-main>
+    <el-footer />
+  </el-container>
 </template>
+
+<script>
+import AppNavigation from '~/components/layout/Navigation'
+
+export default {
+  components: {
+    AppNavigation
+  }
+}
+</script>

38a5f3ac802a0c724601d0523b3a26b750770b06
Kirill Myagkov <kirillmyagkov17@gmail.com>
2020-03-25 12:57:32 +0300

    Add error layout

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="error">
+    <h1 class="error__title">Something went wrong :(</h1>
+    <nuxt-link
+      to="/"
+      tag="el-link"
+    >
+      Back to home page
+    </nuxt-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.error {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 100%;
+}
+
+.error__title {
+  width: 100%;
+  margin-bottom: 1rem;
+  text-align: center;
+}
+</style>

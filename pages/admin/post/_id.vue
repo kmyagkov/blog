d813caa20a9796fa3bd3498927b7c9e207e7db3d
Kirill Myagkov <kirillmyagkov17@gmail.com>
2020-03-30 20:33:17 +0300

    Post edit page

--- pages/admin/post/_id.vue
+++ pages/admin/post/_id.vue
@@ -1,17 +1,101 @@
 <template>
   <div>
-    <el-breadcrumb separator="/">
+    <el-breadcrumb
+      separator="/"
+      class="mb"
+    >
       <el-breadcrumb-item to="/admin/posts">
         Posts
       </el-breadcrumb-item>
-      <el-breadcrumb-item>{{ $route.params.id }}</el-breadcrumb-item>
+      <el-breadcrumb-item>{{ post.title }}</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <el-form
+      ref="form"
+      :model="controls"
+      :rules="rules"
+      label-width="120px"
+      label-position="top"
+      @submit.native.prevent="submit"
+      class="form"
+    >
+      <h1>
+        Edit post
+      </h1>
+      <el-form-item label="Type text (.md / .html)" prop="text">
+        <el-input
+          v-model.trim="controls.text"
+          type="textarea"
+          resize="none"
+          rows="10"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          native-type="submit"
+          :loading="loading"
+        >
+          Save
+        </el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
   layout: 'admin',
-  middleware: ['auth']
+  middleware: ['auth'],
+  async asyncData ({ store, params }) {
+    const post = await store.dispatch('post/fetchAdminById', params.id)
+    return { post }
+  },
+  data () {
+    return {
+      loading: false,
+      controls: {
+        text: '',
+        password: ''
+      },
+      rules: {
+        text: [
+          { required: true, message: 'Text is required', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  validate ({ params }) {
+    return !!params.id
+  },
+  methods: {
+    submit () {
+      this.$refs.form.validate(async (valid) => {
+        if (valid) {
+          this.loading = true
+
+          try {
+            const updatedPost = {
+              text: this.controls.text,
+              id: this.post._id
+            }
+
+            await this.$store.dispatch('post/update', updatedPost)
+            this.$message.success('Post updated')
+            this.loading = false
+          } catch (e) {
+            this.loading = false
+          }
+        }
+      })
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.form {
+  max-width: 600px;
+  margin-top: 1rem;
+}
+</style>
--- store/post.js
+++ store/post.js
@@ -1,19 +1,43 @@
+const posts = [
+  {
+    title: 'Post 1',
+    date: new Date(),
+    views: 0,
+    comments: [],
+    _id: '1'
+  },
+  {
+    title: 'Post 2',
+    date: new Date(),
+    views: 0,
+    comments: [],
+    _id: '2'
+  }
+]
+
 export const actions = {
   async fetchAdmin ({ commit }) {
     return await new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve([{
-          title: 'Post',
-          date: new Date(),
-          views: 0,
-          comments: [],
-          _id: 123123
-        }])
+        resolve(posts)
       }, 1000)
     })
   },
 
   remove ({ commit }, id) {
     console.log(`Post ${id} deleted`)
+  },
+
+  update ({ commit }, { text, id }) {
+    console.log('update')
+  },
+
+  async fetchAdminById ({ commit }, id) {
+    return await new Promise((resolve, reject) => {
+      console.log(posts.find(post => post._id === id))
+      setTimeout(() => {
+        resolve(posts.find(post => post._id === id))
+      }, 1000)
+    })
   }
 }
